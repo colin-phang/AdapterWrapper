79fbc7e4c008274a2b2fad9f245e5be315eacfde
pengguolin <pengguolin@ppmoney.com>
2016-10-26 20:32:36 +0800

    Demo editing

--- app/src/main/java/colin/com/adapterwrappersample/MainActivity.java
+++ app/src/main/java/colin/com/adapterwrappersample/MainActivity.java
@@ -2,12 +2,35 @@ package colin.com.adapterwrappersample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rv;
+    private MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rv = (RecyclerView) findViewById(R.id.rv);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyAdapter();
+
+        rv.setAdapter(adapter);
+        addData();
+    }
+
+    private void addData() {
+        List<String> datas = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            datas.add("Item " + i);
+        }
+        adapter.setDatas(datas);
+        adapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/colin/com/adapterwrappersample/MyAdapter.java
@@ -0,0 +1,49 @@
+package colin.com.adapterwrappersample;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by pengguolin on 2016/10/26.
+ */
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    List<String> mDatas;
+
+
+    public void setDatas(List<String> mDatas) {
+        this.mDatas = mDatas;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_item, viewGroup, false);
+
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        viewHolder.tv.setText(mDatas.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas == null ? 0 : mDatas.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tv;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tv = (TextView) itemView.findViewById(R.id.tv);
+        }
+    }
+}
